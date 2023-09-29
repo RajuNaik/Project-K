7207c44833176321fca18741ade1e41dd85aaa3f
RajuNaik <50011146+RajuNaik@users.noreply.github.com>
2023-09-29 11:30:44 +0530

    Update dev_deploy_script.sh

--- dev_deploy_script.sh
+++ dev_deploy_script.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
 # Azure Databricks CLI setup
-export DATABRICKS_HOST=https://adb-3943185736870612.12.azuredatabricks.net  # Replace with your Databricks workspace URL
-export DATABRICKS_TOKEN=<Your_Databricks_Token>  # Replace with your Databricks API token
-export DATABRICKS_ORG_ID=3943185736870612  # Replace with your organization ID
+export DATABRICKS_HOST=https://adb-3943185736870612.12.azuredatabricks.net  
+export DATABRICKS_TOKEN=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  
+export DATABRICKS_ORG_ID=3943185736870612  
 
 # Databricks workspace parameters
-export WORKSPACE_PATH=/Shared/my_notebooks  # Replace with the desired workspace path
-export NOTEBOOK_FILE=my_notebook.dbc  # Replace with the name of your DBC file
+export WORKSPACE_PATH=/Shared/my_notebooks  
 
 # GitHub token for authentication
 export GITHUB_TOKEN=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
 
-# Import the notebook into the Databricks workspace
-databricks workspace import -l PYTHON -f $NOTEBOOK_FILE $WORKSPACE_PATH/$NOTEBOOK_FILE
+# Clone the repository to access notebooks in the dev branch
+git clone -b dev https://github.com/RajuNaik/Project-K.git dev-repo
 
-# Check the import result
-if [ $? -eq 0 ]; then
-  echo "Deployment to Dev environment succeeded."
-else
-  echo "Deployment to Dev environment failed."
-  exit 1
-fi
+# Navigate to the directory with notebooks
+cd dev-repo/notebooks
+
+# Deploy all notebooks in the directory
+for NOTEBOOK_FILE in *.dbc; do
+  if [ -f "$NOTEBOOK_FILE" ]; then
+    # Import the notebook into the Databricks workspace
+    databricks workspace import -l PYTHON -f "$NOTEBOOK_FILE" "$WORKSPACE_PATH/$NOTEBOOK_FILE"
+
+    # Check the import result
+    if [ $? -eq 0 ]; then
+      echo "Deployment of $NOTEBOOK_FILE to Dev environment succeeded."
+    else
+      echo "Deployment of $NOTEBOOK_FILE to Dev environment failed."
+      exit 1
+    fi
+  fi
+done
